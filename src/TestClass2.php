2590fa74196d236e0346d79ea4e08fec25731435
GBrabyn <gbrabyn@gmail.com>
2020-05-09 01:21:23 +0200

    test 2

---
+++ src/TestClass2.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace GBrabyn\DynamicForms;
+
+/**
+ * Description of TestClass2
+ *
+ * @author GBrabyn
+ */
+class TestClass2 {
+    //put your code here
+}
